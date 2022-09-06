00d41c3af4c29b41980535b4111b619b2a205e44
Grant Cohoe <grant@grantcohoe.com>
2022-09-05 22:50:49 -0400

    ingredient fixes, bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -ex
+
+london setup
+london construction recreate all
+london glassware recreate all
+london ingredient recreate all
+london inventory recreate all
+london lists recreate all
+london recipe recreate all
